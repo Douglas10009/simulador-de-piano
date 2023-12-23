5c2198c133c85e166113ad694041f49545243689
Douglas <douglasevertonev@gmail.com>
2023-12-23 15:41:46 -0300

    fazendo trabalho

---
+++ src/scripts/engine.js
@@ -0,0 +1,41 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input')
+
+let mapedKeys = [];
+let audio = new Audio('./src/tunes/a.wav');
+
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add('active');
+
+    // Depois de um tempo = setTimeout
+    setTimeout(() => {
+        clickedKey.classList.remove('active')
+    }, 150)
+
+
+};
+
+pianoKeys.forEach((key) => {
+    key.addEventListener('click', () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener('keydown', (e) => {
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+})
+
+const handleVolume = (e) => {
+    console.log(e.target.value);
+    audio.volume = e.target.value;
+}
+
+volumeSlider.addEventListener('input', handleVolume);
+
+
