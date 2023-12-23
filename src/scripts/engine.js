28a03715a91e9b94f1ddfa676de62c9f66f226f5
Douglas <douglasevertonev@gmail.com>
2023-12-23 15:58:58 -0300

    esteregg

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,11 +3,15 @@ const volumeSlider = document.querySelector('.volume-slider input');
 const keysCheck = document.querySelector('.keys-check input')
 
 let mapedKeys = [];
-let audio = new Audio('./src/tunes/a.wav');
+let audio = new Audio() ;
+let musica = new Audio('./src/tunes/Erik Satie - Gymnopédie No. 1.mp3')
 
 
+console.log('O que poderia acontecer se `musica` fosse tocada? 0-0');
+
+// Tocar a nota
 const playTune = (key) => {
-    audio.src = `./src/tunes/${key}.wav`;
+    audio.src = `./src/tunes/${key}.wav` || "";
     audio.play();
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`)
@@ -21,6 +25,7 @@ const playTune = (key) => {
 
 };
 
+// Mapeia todas as teclas 
 pianoKeys.forEach((key) => {
     key.addEventListener('click', () => playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
@@ -47,3 +52,8 @@ const showHideKeys = ()=>{
 }
 
 keysCheck.addEventListener('click', showHideKeys)
+
+
+musica.addEventListener('playing', ()=>{
+    console.log('Agora você é um profissa fi! ');
+})
